23fd350199e57bd186c09290b2f3d5c66d8251a2
siddharthmallik <malliksiddharth@gmail.com>
2021-09-02 01:27:38 +0530

    initial commit

---
+++ app/src/main/java/com/example/toggle/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.toggle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    ToggleButton toggleButton;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton = findViewById(R.id.toggleButton);
+        imageView = findViewById(R.id.imageView);
+
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.light_off));
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (toggleButton.isChecked()){
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.light_on));
+
+                }else {
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.light_off));
+                }
+            }
+        });
+    }
+}
